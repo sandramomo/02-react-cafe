68067eec17f9adc84b004e17dfde8b799feece27
Oleksandra <olexandra.momotiuk@gmail.com>
2025-09-27 15:49:24 +0300

    Finish hw

---
+++ src/components/App.tsx
@@ -0,0 +1,50 @@
+
+import css from'../styles/App.module.css'
+import CafeInfo from './CafeInfo'
+import VoteOptions from './VoteOptions'
+import VoteStatus from './VoteStatus'
+import type { VoteType,  Votes } from '../types/votes'
+import { useState } from 'react'
+
+
+
+function App() {
+   const  [votes, setVotes] = useState<Votes>({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+   });
+  
+function handleVote(type:VoteType): void {
+    console.log('You voted:', type, votes);
+   setVotes(prev => ({
+      ...prev,
+     [type]: prev[type] + 1, 
+      
+   })
+   );
+  
+  }
+
+  function resetVotes(): void {
+    console.log('Reset votes');
+    setVotes({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+    })
+  }
+ const totalVotes = votes.good + votes.neutral + votes.bad
+  const positiveRate = totalVotes
+    ? Math.round((votes.good / totalVotes) * 100)
+    : 0
+  return (
+      <div className={css.app}>
+        <CafeInfo />
+      <VoteOptions onVote={handleVote} onReset={resetVotes} canReset={totalVotes>0} />
+      <VoteStatus votes={ votes } totalVotes={totalVotes} positiveRate={ positiveRate} />
+      </div>
+  )
+}
+
+export default App
---
+++ src/components/Notification.tsx
@@ -0,0 +1,4 @@
+import css from '../styles/Notification.module.css'
+export default function Notification() {
+    return <p className={css.message}>No feedback yet</p>
+}
